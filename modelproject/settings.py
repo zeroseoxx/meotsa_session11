5139628376a01f812a429cab839ab0c900c0106d
zeroseoxx <soya117@hufs.ac.kr>
2025-07-13 00:19:27 +0900

    메인 레포로 통합

--- modelproject/settings.py
+++ modelproject/settings.py
@@ -95,12 +95,12 @@ WSGI_APPLICATION = "modelproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
@@ -221,3 +221,17 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+import pymysql
+
+pymysql.install_as_MySQLdb()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'), # DB(스키마) 이름
+        'USER': config('DB_USER'), # 유저 이름 (root)
+        'PASSWORD': config('DB_PASSWORD'), # DB 비밀번호
+        'HOST': config('DB_HOST'), # DB 엔드포인트
+        'PORT': 3306,
+    }
+}
